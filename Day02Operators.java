07d935d44319430e4669630b3c28bbf339800b41
mattjass <jassx011@umn.edu>
2016-07-06 17:25:07 -0400

    Final Product

--- Day02Operators.java
+++ Day02Operators.java
@@ -2,24 +2,25 @@
 
 Link: https://www.hackerrank.com/challenges/30-operators
 
-Objective
-In this challenge, you'll work with arithmetic operators. Check out the Tutorial tab for learning materials and an instructional video!
+Objective:
+In this challenge, you'll work with arithmetic operators.
+Check out the Tutorial tab for learning materials and an instructional video!
 
-Task
-Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost.
+Task:
+Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip),
+and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost.
 
 Note: Be sure to use precise values for your calculations, or you may end up with an incorrectly rounded result!
 
-Input Format
-
+Input Format:
 There are  lines of numeric input:
-The first line has a double,  (the cost of the meal before tax and tip).
-The second line has an integer,  (the percentage of  being added as tip).
-The third line has an integer,  (the percentage of  being added as tax).
+    The first line has a double,  (the cost of the meal before tax and tip).
+    The second line has an integer,  (the percentage of  being added as tip).
+    The third line has an integer,  (the percentage of  being added as tax).
 
-Output Format
-
+Output Format:
 Print , where  is the rounded integer result of the entire bill ( with added tax and tip).
+
  */
 
 import java.util.Scanner;
@@ -27,21 +28,35 @@ import java.util.Scanner;
 public class Day02Operators {
 
     public static void main(String[] args) {
+
+        System.out.println("Meal Calculator");
         Scanner scan = new Scanner(System.in);
+
+
+        System.out.print("Original Meal Price: ");
         double mealCost = scan.nextDouble(); // original meal price
+        System.out.println();
+
+        System.out.print("Tip Percentage: ");
         int tipPercent = scan.nextInt(); // tip percentage
+        System.out.println();
+
+        System.out.print("Tax percentage: ");
         int taxPercent = scan.nextInt(); // tax percentage
+        System.out.println();
+
         scan.close();
 
-        // Write your calculation code here.
+        // Calculations
         double tip = (double)tipPercent * mealCost / 100;
         double tax = (double)taxPercent * mealCost / 100;
 
-        // cast the result of the rounding operation to an int and save it as totalCost
+        // Cast the result of the rounding operation to an int and save it as totalCost
         int totalCost = (int) Math.round(mealCost+tip+tax);
 
         // Print your result
         System.out.println("The total meal cost is " + totalCost + " dollars.");
-    }
 
-}
+    } // end main
+
+} // Day02Operators
--- Day03IntroToConditionalStatements.java
+++ Day03IntroToConditionalStatements.java
@@ -2,10 +2,10 @@
 
 Link: https://www.hackerrank.com/challenges/30-conditional-statements
 
-Objective
+Objective:
 In this challenge, we're getting started with conditional statements. Check out the Tutorial tab for learning materials and an instructional video!
 
-Task
+Task:
 Given an integer, , perform the following conditional actions:
 
 If n is odd, print Weird
@@ -14,14 +14,13 @@ If n is even and in the inclusive range of 6 to 20, print Weird
 If n is even and greater than 20, print Not Weird
 Complete the stub code provided in your editor to print whether or not  is weird.
 
-Input Format
-
+Input Format:
 A single line containing a positive integer, .
 
-Constraints
+Constraints:
 1 <= n <= 100
 
-Output Format
+Output Format:
 
 Print Weird if the number is weird; otherwise, print Not Weird.
  */
@@ -57,6 +56,6 @@ public class Day03IntroToConditionalStatements {
             } // end else
         } // end else
         System.out.println(ans);
-    }
+    } // end main
 
-}
+} // end Day 03
