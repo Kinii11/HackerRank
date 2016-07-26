8c2a742e737452cc0e68be26c2b4b8679f131774
mattjass <jassx011@umn.edu>
2016-07-26 18:28:45 -0500

    First Attempt

---
+++ Day05Loops.java
@@ -0,0 +1,34 @@
+/* Hacker Rank: Day 5 - Matt Jass
+
+Link: https://www.hackerrank.com/challenges/30-loops
+
+Objective:
+In this challenge, we're going to use loops to help us do some simple math.
+Check out the Tutorial tab to learn more.
+
+Task:
+Given an integer, N, print its first 10 multiples. Each multiple N x i (where 1 <= i <= 10 )
+should be printed on a new line in the form: N x i = result.
+
+Input Format:
+A single integer, N.
+
+Constraints:
+2 <= N <= 20
+
+Output Format:
+Print 10 lines of output; each line i (where 1 <= i <= 10) contains the restulf of N x i in the form:
+N x i = result.
+
+ */
+import java.io.*;
+import java.util.*;
+
+public class Day05Loops {
+
+    public class Solution {
+    } // end Solution
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    } // end main
+} // end Day05Loops
