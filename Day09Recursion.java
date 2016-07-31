b4660b51d0b3190c038c6403123d3b2c0085830d
mattjass <jassx011@umn.edu>
2016-07-31 16:25:24 -0500

    Initial Commit
    Set up objectives and structure.

---
+++ Day09Recursion.java
@@ -0,0 +1,35 @@
+/* Hacker Rank: Day 9 - Matt Jass
+
+Link: https://www.hackerrank.com/challenges/30-recursion
+
+Objective:
+Today, we're learning and practicing an algorithmic concept called Recursion.
+Check out the Tutorial tab for learning materials and an instructional video!
+
+Task:
+Write a factorial function that takes a positive integer, N as a parameter and prints the result of N! (N factorial).
+
+Note: If you fail to use recursion or fail to name your recursive function factorial or Factorial,
+you will get a score of 0.
+
+Input Format:
+A single integer, N (the argument to pass to factorial).
+
+Constraints:
+2 <= N <= 12
+
+Your submission must contain a recursive function named factorial.
+
+Output Format:
+Print a single integer denoting N!.
+
+ */
+
+public class Day09Recursion {
+
+    public class Factorial {} // end Factorial
+
+    public static void main (String[] args) {
+
+    } // end main
+} // end Day09Recursion
