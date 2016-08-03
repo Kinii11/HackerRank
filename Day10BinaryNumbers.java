5c1fbe04938e79381fccde4f71dc6deeb3329e18
mattjass <jassx011@umn.edu>
2016-08-02 21:42:55 -0500

    Initial Commit
    Set up Objectives and Tasks

---
+++ Day10BinaryNumbers.java
@@ -0,0 +1,27 @@
+/* HackerRank Day 10 - Matt Jass
+
+Link: https://www.hackerrank.com/challenges/30-binary-numbers
+
+Objective:
+Today, we're working with binary numbers.
+Check out the Tutorial tab for learning materials and an instructional video!
+
+Task:
+Given a base-10 integer, n, convert it to binary (base-2). Then find and print the base-10 integer denoting the
+maximum number of consecutive 1's in n's binary representation.
+
+Input Format:
+A single integer, n.
+
+Constraints:
+1 <= n <= 10^6
+
+Output Format
+Print a single base-10 integer denoting the maximum number of consecutive 1's in the binary representation of n.
+
+ */
+
+
+public class Day10BinaryNumbers {
+
+} // end Day10BinaryNumbers
