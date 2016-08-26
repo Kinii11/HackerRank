105e527c1ebf41c903a6dd5e6bbcf21c751365c8
mattjass <jassx011@umn.edu>
2016-08-26 15:34:37 -0500

    Added: Get input from user Conversion to Binary Calculation of Maximum Ones Print output

--- Day10BinaryNumbers.java
+++ Day10BinaryNumbers.java
@@ -22,6 +22,37 @@ Print a single base-10 integer denoting the maximum number of consecutive 1's in
  */
 
 
+import java.util.Scanner;
+
 public class Day10BinaryNumbers {
+
+    public static void main (String[] args) {
+
+        // Get input from user
+        Scanner keyboard = new Scanner(System.in);
+        // System.out.print("Enter an Integer to Convert: ");
+        int inputNumber = keyboard.nextInt();
+        keyboard.close();
+
+        // Convert to Binary
+        String binaryNumberString = Integer.toBinaryString(inputNumber).toString();
+
+        // Calculate Maximum number of ones in a row
+        int maxNumberOfOnes = 0, currentNumber = 0;
+        for (int i = 0; i < binaryNumberString.length(); i++) {
+
+            if (binaryNumberString.equals("1")) {
+                currentNumber++;
+            } // end if
+            if (currentNumber > maxNumberOfOnes) {
+                maxNumberOfOnes = currentNumber;
+                currentNumber = 0;
+            } // end if
+
+        } // end for
+        // Print output
+        // System.out.print("Max number = ");
+        System.out.println(maxNumberOfOnes);
+    } // end main
 
 } // end Day10BinaryNumbers
